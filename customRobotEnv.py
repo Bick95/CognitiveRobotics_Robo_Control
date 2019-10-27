b9f7cb25df1d03ce73504f0ddaf7a62d79fcdc3b
Bick95 <daniel.bick@live.de>
2019-10-27 20:39:07 +0100

    Initial Commit
    main.py implements the logic of the training. It creates the environment (via which the simulation is controlled) and passes it to the PPO algorithm provided by the stable-baselines library & finally starts training.
    
    customRobotEnv.py contains the implementation of the Gym environment to control the pybullet simulation. It is an adaption of the kukaGymEnv.py, which was copied from the pybullet-library examples. From this file, mainly the main logic defining the Gym env will be kept, but the environment's contents are going to be replaced such that training is performed on a model of a Franka Emika Panda.
    
    kukaGymEnv.py is the original Gym environment from which our own one is going to be derived. It only serves as inspiration, but is not in use. 
    
    Required software can be installed by executing the following pip commands:
    
    pip3 install tensorflow
    pip3 install pybullet
    pip3 install stable-baselines

---
+++ customRobotEnv.py
@@ -0,0 +1,250 @@
+import os, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+print("current_dir=" + currentdir)
+os.sys.path.insert(0, currentdir)
+
+import math
+import gym
+from gym import spaces
+from gym.utils import seeding
+import numpy as np
+import time
+import pybullet as p
+from pybullet_envs.bullet import kuka
+import random
+import pybullet_data
+from pkg_resources import parse_version
+from scipy.spatial import distance
+
+largeValObservation = 100
+
+RENDER_HEIGHT = 720
+RENDER_WIDTH = 960
+
+# TODO: explore setRealTimeSimulation
+# TODO: put in franka model and free from Kuka stuff
+
+class CustomRobotEnv(gym.Env):
+    metadata = {'render.modes': ['human', 'rgb_array'], 'video.frames_per_second': 50}
+
+    def __init__(self,
+                 urdfRoot=pybullet_data.getDataPath(),
+                 actionRepeat=1,
+                 isEnableSelfCollision=True,
+                 renders=False,
+                 isDiscrete=False,
+                 maxSteps=1000):
+        # print("KukaGymEnv __init__")
+        self._isDiscrete = isDiscrete
+        self._timeStep = 1. / 240.
+        self._urdfRoot = urdfRoot
+        self._actionRepeat = actionRepeat
+        self._isEnableSelfCollision = isEnableSelfCollision
+        self._observation = []
+        self._envStepCounter = 0
+        self._renders = renders
+        self._maxSteps = maxSteps
+        self.terminated = 0
+        self._cam_dist = 1.3
+        self._cam_yaw = 180
+        self._cam_pitch = -40
+        self._last_dist_to_obj = 0.0
+
+        self._p = p
+        if self._renders:
+            cid = p.connect(p.SHARED_MEMORY)
+            if (cid < 0):
+                cid = p.connect(p.GUI)
+            p.resetDebugVisualizerCamera(1.3, 180, -41, [0.52, -0.2, -0.33])
+        else:
+            p.connect(p.DIRECT)
+        # timinglog = p.startStateLogging(p.STATE_LOGGING_PROFILE_TIMINGS, "kukaTimings.json")
+        self.seed()
+        self.reset()
+        observationDim = len(self.getExtendedObservation())
+        # print("observationDim")
+        # print(observationDim)
+
+        observation_high = np.array([largeValObservation] * observationDim)
+        if (self._isDiscrete):
+            self.action_space = spaces.Discrete(7)
+        else:
+            action_dim = 3+1
+            self._action_bound = 1
+            action_high = np.array([self._action_bound] * action_dim)
+            self.action_space = spaces.Box(-action_high, action_high)
+        self.observation_space = spaces.Box(-observation_high, observation_high)
+        self.viewer = None
+
+    def reset(self):
+        # print("KukaGymEnv _reset")
+        self.terminated = 0
+        p.resetSimulation()
+        p.setPhysicsEngineParameter(numSolverIterations=150)
+        p.setTimeStep(self._timeStep)
+        p.loadURDF(os.path.join(self._urdfRoot, "plane.urdf"), [0, 0, -1])
+
+        p.loadURDF(os.path.join(self._urdfRoot, "table/table.urdf"), 0.5000000, 0.00000, -.820000,
+                   0.000000, 0.000000, 0.0, 1.0)
+
+        xpos = 0.55 + 0.12 * random.random()
+        ypos = 0 + 0.2 * random.random()
+        ang = 3.14 * 0.5 + 3.1415925438 * random.random()
+        orn = p.getQuaternionFromEuler([0, 0, ang])
+        self.blockUid = p.loadURDF(os.path.join(self._urdfRoot, "block.urdf"), xpos, ypos, -0.15,
+                                   orn[0], orn[1], orn[2], orn[3])
+
+        p.setGravity(0, 0, -10)
+        self._kuka = kuka.Kuka(urdfRootPath=self._urdfRoot, timeStep=self._timeStep)
+        self._envStepCounter = 0
+        p.stepSimulation()
+        self._observation = self.getExtendedObservation()
+        return np.array(self._observation)
+
+    def __del__(self):
+        p.disconnect()
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def getExtendedObservation(self):
+
+        self._observation = []
+
+        # Robot's joint space pos
+        jointPos = self._kuka.jointPositions
+
+        # Block's Cartesian position
+        blockPos, blockOrn = p.getBasePositionAndOrientation(self.blockUid)
+
+        self._observation.extend(blockPos)
+        self._observation.extend(jointPos)
+
+        return self._observation
+
+    def step(self, action):
+        repetition = 1
+        if self._isDiscrete:
+            dv = 0.005
+            dx = [0, -dv, dv, 0, 0, 0, 0][action]
+            dy = [0, 0, 0, -dv, dv, 0, 0][action]
+            da = [0, 0, 0, 0, 0, -0.05, 0.05][action]
+            f = 0.3
+            realAction = [dx, dy, -0.002, da, f, max(1, repetition)]
+        else:
+            repetition = int(action[-1])
+            # print("action[0]=", str(action[0]))
+            dv = 0.005
+            dx = action[0] * dv
+            dy = action[1] * dv
+            da = action[2] * 0.05
+            f = 0.3
+            realAction = [dx, dy, -0.002, da, f, max(1, repetition)]
+        return self.step2(realAction)
+
+    def step2(self, action):
+        for i in range(action[-1]):  # self._actionRepeat
+            self._kuka.applyAction(action)
+            p.stepSimulation()
+            if self._termination:
+                break
+            self._envStepCounter += 1
+        if self._renders:
+            time.sleep(self._timeStep)
+        self._observation = self.getExtendedObservation()
+
+        # print("self._envStepCounter")
+        # print(self._envStepCounter)
+
+        done = self._termination
+
+        reward = self._reward()
+
+        return np.array(self._observation), reward, done, {}
+
+    def render(self, mode="rgb_array", close=False):
+        if mode != "rgb_array":
+            return np.array([])
+
+        base_pos, orn = self._p.getBasePositionAndOrientation(self._kuka.kukaUid)
+        view_matrix = self._p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=base_pos,
+                                                                distance=self._cam_dist,
+                                                                yaw=self._cam_yaw,
+                                                                pitch=self._cam_pitch,
+                                                                roll=0,
+                                                                upAxisIndex=2)
+        proj_matrix = self._p.computeProjectionMatrixFOV(fov=60,
+                                                         aspect=float(RENDER_WIDTH) / RENDER_HEIGHT,
+                                                         nearVal=0.1,
+                                                         farVal=100.0)
+        (_, _, px, _, _) = self._p.getCameraImage(width=RENDER_WIDTH,
+                                                  height=RENDER_HEIGHT,
+                                                  viewMatrix=view_matrix,
+                                                  projectionMatrix=proj_matrix,
+                                                  renderer=self._p.ER_BULLET_HARDWARE_OPENGL)
+        # renderer=self._p.ER_TINY_RENDERER)
+
+        rgb_array = np.array(px, dtype=np.uint8)
+        rgb_array = np.reshape(rgb_array, (RENDER_HEIGHT, RENDER_WIDTH, 4))
+
+        rgb_array = rgb_array[:, :, :3]
+        return rgb_array
+
+    @property
+    def _termination(self):
+        # print (self._kuka.endEffectorPos[2])
+        state = p.getLinkState(self._kuka.kukaUid, self._kuka.kukaEndEffectorIndex)
+        actualEndEffectorPos = state[0]
+
+        # print("self._envStepCounter")
+        # print(self._envStepCounter)
+        if self.terminated or self._envStepCounter > self._maxSteps:
+            self._observation = self.getExtendedObservation()
+            return True
+        maxDist = 0.005
+        closestPoints = p.getClosestPoints(self._kuka.trayUid, self._kuka.kukaUid, maxDist)
+
+        blockPos, blockOrn = p.getBasePositionAndOrientation(self.blockUid)
+        gripperPos, gripperOrn = p.getLinkState(self._kuka.kukaUid, self._kuka.kukaGripperIndex)[0:2]
+
+        # Implements: numpy.sqrt(numpy.sum((numpy.array(a)-numpy.array(b))**2))
+        cartDistance = distance.euclidean(blockPos, gripperPos)
+
+        if cartDistance < 0.2:
+            print('##########\n'*2 + 'GOAL REACHED!' + '##########\n'*2)
+            self.terminated = 1
+            return True
+        return False
+
+    def _reward(self):
+
+        blockPos, blockOrn = p.getBasePositionAndOrientation(self.blockUid)
+        gripperPos, gripperOrn = p.getLinkState(self._kuka.kukaUid, self._kuka.kukaGripperIndex)[0:2]
+
+        # Implements: numpy.sqrt(numpy.sum((numpy.array(a)-numpy.array(b))**2))
+        cartDistance = distance.euclidean(blockPos, gripperPos)
+
+        reward = self._last_dist_to_obj - cartDistance
+        reward *= (10000 if reward > 0 else 50000)
+        self._last_dist_to_obj = cartDistance
+
+        print('Reward parts:')
+        print('Dist-part: ' + str(reward))
+
+        if blockPos[2] > 0.2:
+            # According to provided implementation: Gripper has reached target!
+            reward = reward + 10000 - self._envStepCounter
+            print("successfully grasped a block!!!")
+
+        print('Current step-ctr-subtraction: ' + str(self._envStepCounter))
+
+        print('Reward: ' + str(reward))
+        return reward
+
+    if parse_version(gym.__version__) < parse_version('0.9.6'):
+        _render = render
+        _reset = reset
+        _seed = seed
+        _step = step
---
+++ main.py
@@ -0,0 +1,42 @@
+import gym
+import tensorflow as tf
+from stable_baselines import PPO2
+
+# Environment related
+#from pybullet_envs.bullet.kukaGymEnv import KukaGymEnv
+from customRobotEnv import CustomRobotEnv
+from stable_baselines.common.vec_env import DummyVecEnv
+
+# Create and vectorize Environment
+env_name = "CustomRobotEnv"
+env = eval(env_name + '(renders=True)')  # <-- Allows for making the Env-name variable: returns: KukaGymEnv(renders=True) (as used in line below)
+#env = KukaGymEnv(renders=True)          # was initially: env = gym.make('CartPole-v1')
+env = DummyVecEnv([lambda: env])         # The algorithms require a vectorized environment to run, hence vectorize
+
+# Create custom NN architecture
+policy_kwargs = dict(act_fun=tf.nn.tanh, net_arch=[75, 75])
+
+# Create the PPO agent
+model = PPO2("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1)  # env was before string: "CartPole-v1"
+
+# Retrieve the environment
+env = model.get_env()
+
+# Train the agent
+model.learn(total_timesteps=1000000)
+
+# Save the agent
+model.save("ppo2-" + env_name)
+
+# Show 1000 test iterations to user after training is done
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
+# Demonstrate deletion of model
+del model
+
+# Demonstrating loading of model
+model = PPO2.load("ppo2-" + env_name)
