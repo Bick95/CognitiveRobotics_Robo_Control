d5c365709b900ab8efb0aa346324a1e35a1055f6
Bick95 <s3145697@pg-interactive.hpc.rug.nl>
2019-11-07 12:40:39 +0100

    Added Peregrine bash script

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=05:00:00
+#SBATCH --job-name=frnk6-5
+#SBATCH --mail-type=BEGIN,END,FAILL
+#SBATCH --mail-user=d.bick@student.rug.nl
+#SBATCH --output=/dev/null
+#SBATCH --mem=10GB
+#SBATCH --nodes=1
+#SBATCH --ntasks=12
+module load Python/3.6.4-foss-2019a
+python main.py -p ParameterSettings/params_6.json
