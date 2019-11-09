5a1ceb61fe56f1c98f17f40302c18aa01d00f967
Bick95 <daniel.bick@live.de>
2019-11-09 18:24:55 +0100

    Updated the functionality for running trained model.

--- run_trained_model.py
+++ run_trained_model.py
@@ -8,18 +8,24 @@ from stable_baselines.common.vec_env import DummyVecEnv
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        path = sys.argv[1]
+        path = sys.argv[1]  # Provide the path to your custom model
     else:
-        path = "Results/PandaController_2019_08_11__15_41_05__262730fzyxnprhgl/final_model.zip"
+    
+        path = "Evaluation_CognitiveRobotiRobo_Control/Results/PPO2/PandaController_2019_08_11__15_41_05__262730fzyxnprhgl/final_model.zip"
+    
     if len(sys.argv) > 2:
         iterations = sys.argv[2]
     else:
         iterations = 200000
 
-    env = PandaRobotEnv(renders=True, fixedActionRepetitions=True)
+    env = PandaRobotEnv(renders=True, fixedActionRepetitions=True)  # Todo: could be adjusted to be dependent on parameters used during training.
     env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run, hence vectorize
-
-    model = PPO2.load(path)
+    
+    try:
+        model = PPO2.load(path)
+    except ValueError:
+        print('\nError: Make sure to have the pre-trained models available or to provide a valid path to a custom model as an argument.\n')
+        sys.exit()
 
     # Enjoy trained agent
     obs = env.reset()
