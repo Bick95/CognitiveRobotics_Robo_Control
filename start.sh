7edd93ca3dd48559d076861ee9b3cb02ada736a2
Bick95 <daniel.bick@live.de>
2019-11-16 00:25:42 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 #SBATCH --time=05:00:00
 #SBATCH --job-name=frnk6-5
 #SBATCH --mail-type=BEGIN,END,FAILL
-#SBATCH --mail-user=d.bick@student.rug.nl
 #SBATCH --output=/dev/null
 #SBATCH --mem=10GB
 #SBATCH --nodes=1
